aca7f7d94531fc27033b20ab833a78c68762528f
jack.cheng <jack.cheng@davionlabs.com>
2023-08-20 19:18:05 +0800

    build: up deps

--- auto_release_cocoapods.sh
+++ auto_release_cocoapods.sh
@@ -3,7 +3,8 @@
 
 pod spec lint
 
-pod trunk register flywithbug@gmail.com 'flywithbug' --description=路由组件 
+pod trunk register flywithbug@gmail.com flywithbug  --verbose
+#邮箱验证
 
 #上传
 pod trunk push FBRouter.podspec --allow-warnings
